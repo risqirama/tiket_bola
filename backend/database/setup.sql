76414d55bd8115808cfcda70ad61ed08998bf5da
risqirama <153357242+risqirama@users.noreply.github.com>
2025-01-18 13:57:42 +0700

    Update setup.sql

--- backend/database/setup.sql
+++ backend/database/setup.sql
@@ -1,12 +1,11 @@
 -- Drop tables if they exist
 DROP TABLE IF EXISTS bookings;
-DROP TABLE IF EXISTS concerts;
+DROP TABLE IF EXISTS matchs;
 
 -- Create concerts table
 CREATE TABLE concerts (
     id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
-    artist VARCHAR(255) NOT NULL,
     venue VARCHAR(255) NOT NULL,
     date DATETIME NOT NULL,
     price DECIMAL(10,2) NOT NULL,
@@ -17,7 +16,7 @@ CREATE TABLE concerts (
 -- Create bookings table
 CREATE TABLE bookings (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    concert_id INT NOT NULL,
+    match_id INT NOT NULL,
     full_name VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
     phone_number VARCHAR(20) NOT NULL,
@@ -25,14 +24,14 @@ CREATE TABLE bookings (
     total_price DECIMAL(10,2) NOT NULL,
     booking_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     status VARCHAR(20) DEFAULT 'pending',
-    FOREIGN KEY (concert_id) REFERENCES concerts(id)
+    FOREIGN KEY (match_id) REFERENCES matchs(id)
 );
 
 -- Insert sample concert data
-INSERT INTO concerts (title, artist, venue, date, price, available_tickets, image_url) VALUES
-('The Eras Tour', 'Taylor Swift', 'SoFi Stadium, Los Angeles', '2025-03-15 19:00:00', 399.99, 100, '/images/taylorswift.jpg'),
-('Renaissance World Tour', 'Beyonce', 'MetLife Stadium, New Jersey', '2025-04-01 20:00:00', 299.99, 150, '/images/beyonce.jpeg'),
-('Music Of The Spheres Tour', 'Coldplay', 'Wembley Stadium, London', '2025-05-20 19:30:00', 199.99, 200, '/images/coldplay.png'),
-('Born Pink World Tour', 'BLACKPINK', 'Tokyo Dome, Japan', '2025-06-10 18:00:00', 249.99, 180, '/images/blackpink.jpg'),
-('Mercury World Tour', 'Imagine Dragons', 'Madison Square Garden, NYC', '2025-07-05 19:00:00', 149.99, 250, '/images/imaginedragon.jpg'),
-('24K Magic World Tour', 'Bruno Mars', 'The O2, London', '2025-08-15 20:00:00', 179.99, 220, '/images/brunomars.jpg');
+INSERT INTO concerts (title, venue, date, price, available_tickets, image_url) VALUES
+('INDONESIA VS BAHRAIN', 'GELORA BUNG KARNO', '2025-03-15 19:00:00', 500000.00, 14234, '/images/idnvsbhr.jpg'),
+('ARAB SAUDI VS INDONESIA',  'LUSAIL STADIUM, New Jersey', '2025-04-01 20:00:00', 450000.00, 36750, '/images/idnvsarb.jpeg'),
+('INDONESIA VS JEPANG', 'JAKARTA INTERNATIONAL STADIUM', '2025-05-20 19:30:00', 750000.00, 81000, '/images/idnvsjpg.jpg'),
+('CHINA VS INDONESIA', 'QINGDAO YOUTH STADIUM', '2025-06-10 18:00:00', 300000.00, 180, '/images/idnvschn.jpg'),
+('INDONESIA VS VIETNAM', 'GELORA BUNG KARNO', '2025-07-05 19:00:00', 250000.00, 250, '/images/idnvsviet.jpeg'),
+('INDONESIA VS ARGENTINA', 'GELORA BUNG KARNO', '2025-08-15 20:00:00', 3000000.00, 220, '/images/idnvsarg.jpeg');
